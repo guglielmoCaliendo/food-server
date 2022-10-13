b58b25cf2101587497b810bfc7f5d276b54b113d
Guglielmo Caliendo <guglielmocaliendo92@gmail.com>
2022-10-13 16:20:31 -0400

    update

--- src/db.js
+++ src/db.js
@@ -1,58 +1,75 @@
 require('dotenv').config();
+const { DataTypes } = require('sequelize');
 const { Sequelize } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
 const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
 
 const sequelize = new Sequelize(
-  `postgres://postgres:Xblasters3@localhost/food`,
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
   {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
-)
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch((err) => {
-    console.error('Unable to connect to the database:', err);
-  });
-const basename = path.basename(__filename);
+);
 
-const modelDefiners = [];
+const Diet = sequelize.define(
+  'diet',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
-// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-fs.readdirSync(path.join(__dirname, '/models'))
-  .filter(
-    (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-  )
-  .forEach((file) => {
-    modelDefiners.push(require(path.join(__dirname, '/models', file)));
-  });
-
-// Injectamos la conexion (sequelize) a todos los modelos
-modelDefiners.forEach((model) => model(sequelize));
-// Capitalizamos los nombres de los modelos ie: product => Product
-let entries = Object.entries(sequelize.models);
-let capsEntries = entries.map((entry) => [
-  entry[0][0].toUpperCase() + entry[0].slice(1),
-  entry[1],
-]);
-sequelize.models = Object.fromEntries(capsEntries);
-
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
-const { Recipe, Diet } = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+const Recipe = sequelize.define(
+  'recipe',
+  {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      defaultValue: '',
+    },
+    image: {
+      type: DataTypes.TEXT,
+    },
+    healthScore: {
+      type: DataTypes.INTEGER,
+      defaultValues: 0,
+    },
+    steps: {
+      type: DataTypes.TEXT,
+      defaultValue: '',
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 Recipe.belongsToMany(Diet, { through: 'RecipeDiet' });
 Diet.belongsToMany(Recipe, { through: 'RecipeDiet' });
 
 module.exports = {
-  ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
+  Recipe,
+  Diet,
   conn: sequelize, // para importart la conexión { conn } = require('./db.js');
 };
